ed9b567dc92c695f2a2480123cdc0bfa674a19ba
HosSamNeo <81299330+HosSamNeo@users.noreply.github.com>
2022-12-20 12:26:09 +0200

    .

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import React from 'react';
+
+const index = () => {
+  return (
+   <>
+    <Head>
+      <title>Amazon Clone</title>
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+    </Head>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+    </h1>  
+  </>
+  )
+}
+
+export default index
