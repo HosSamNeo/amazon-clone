bbf4b10d4eb61f8566e518001c52f6e871719680
HosSamNeo <81299330+HosSamNeo@users.noreply.github.com>
2022-12-22 17:26:14 +0200

    axios
    axios

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Header from '../Components/Header';
 import Banner from '../Components/Banner';
 import ProductFeed from '../Components/ProductFeed';
+import axios from 'axios';
 
 export default function index({ products }) {
   return (
@@ -20,7 +21,7 @@ export default function index({ products }) {
 
 export async function getServerSideProps(context){
   
-  const products = await fetch('http://localhost:3000/api/products')
+  const products = await axios.get('http://localhost:3000/api/products')
   .then(res => res.json());
 
   return {
